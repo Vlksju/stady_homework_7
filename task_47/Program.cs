4e0002360545a4a738f544a44e242590e69bf359
Vlksju <xenya.vol2017@yandex.ru>
2022-09-19 20:10:13 +0500

    Solved task 47

---
+++ task_47/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Программа которая выводит двумерный массив m*n, заполненный случайными вещественными числами!");
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = rand.Next(-5,5) + rand.NextDouble ();
+        Console.Write (matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
