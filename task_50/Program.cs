e7ea9a91c727016602f26a19f864854aaa843d68
Vlksju <xenya.vol2017@yandex.ru>
2022-09-19 20:20:18 +0500

    Solved task 50

---
+++ task_50/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Программа проверяет наличие элемента в массиве по его индексам");
+
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Исходный массив = ");
+
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(-10, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите номер строки = ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер столбца = ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if ((m1 <= m) & (n1 <= n))
+{
+    Console.Write($"Элемент {matrix[m1-1, n1-1]}");
+}
+else { Console.Write("Элемента не существует!"); }
